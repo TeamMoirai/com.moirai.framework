7db1d0434ba6d641b959151c14e26c1120a65437
Lx34r <zyssmart@live.com>
2026-06-25 17:17:33 +0800

    add StartLogicExample.cs

---
+++ Templates~/@Requirements/Scripts/GameLogic/StartLogicExample.cs
@@ -0,0 +1,12 @@
+using Moirai.Atropos;
+
+namespace GameLogic
+{
+    public sealed partial class HotfixEntry
+    {
+        private static partial void StartGameLogic()
+        {
+            Log.Info("Starting GameLogic...");
+        }
+    }
+}
