055d6d869c8403ff78f144411aec6d914d8c6eff
Lx34r <zyssmart@live.com>
2026-07-08 02:24:40 +0800

    add UI use sample

---
+++ Templates~/@Requirements/Scripts/GameLogic/UI/StartScreen.cs
@@ -0,0 +1,14 @@
+﻿using Moirai.Atropos.UI;
+using UnityEngine;
+
+namespace GameLogic.UI
+{
+	[Window(UILayer.UI)]
+	public partial class StartScreen : UIWindow
+	{
+		protected override void OnRefresh()
+		{
+			_tmpInfo.text = (string)UserData;
+		}
+	}
+}
