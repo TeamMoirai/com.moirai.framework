5fecd530d2019e7cb9962097370a19867d9858b5
Lx34r <zyssmart@live.com>
2026-07-08 02:24:40 +0800

    add UI use sample

--- Templates~/@Requirements/Scripts/GameLogic/StartLogicExample.cs
+++ Templates~/@Requirements/Scripts/GameLogic/StartLogicExample.cs
@@ -1,13 +1,26 @@
 using Moirai.Atropos;
+using GameLogic.UI;
+using Moirai.Atropos.ConfigTable;
 
 namespace GameLogic
 {
-    // ReSharper disable once ClassNeverInstantiated.Global
     public sealed partial class HotfixEntry
     {
         private static partial void StartGameLogic()
         {
             Log.Info("Starting GameLogic...");
+            GameModule.UI.ShowUIAsync<StartScreen>("StartScreen", GetWindowLocation("start"), false, "Start Screen");
+        }
+
+        /// <summary>
+        /// 从配置表获取弹窗资产的位置
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private static string GetWindowLocation(string id)
+        {
+            // Log.Info($"Load UI: {id}");
+            return ConfigMgr.Instance.GetUIWindowLocation(id);
         }
     }
 }
