4b4013d98b6d6f4a76c2ef106bb9d60579ca2fd0
Lx34r <zyssmart@live.com>
2026-07-18 12:46:26 +0800

    Move command line parameter logic to Debugger and Localization modules

--- Runtime/Core/Utility/CommandLineUtility.cs
+++ Runtime/Core/Utility/CommandLineUtility.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Moirai.Atropos
 {
@@ -80,54 +79,6 @@ namespace Moirai.Atropos
 
                 return s_ArgumentLine;
             }
-        }
-
-        // ------------------------------ 自定义参数 ------------------------------
-        
-        /// <summary>
-        /// 常见的参数
-        /// </summary>
-        public static partial class CommonCommands
-        {
-            /// <summary>
-            /// 强制系统语言
-            /// </summary>
-            public const string MODULE_FORCE_LANGUAGE = "-force-language";
-
-            /// <summary>
-            /// 显示调试信息
-            /// </summary>
-            public const string MODULE_SHOW_DEBUGGER = "-show-debugger";
-        }
-        
-        /// <summary>
-        /// 检查命令行中的 <see cref="CommonCommands.MODULE_FORCE_LANGUAGE"/> 参数，如果找到，则返回该语言
-        /// </summary>
-        /// <remarks>传入 Language Name 或 Code</remarks>
-        /// <returns>如果存在，则会优先使用参数的语言</returns>
-        /// <example>强制语言为汉语 => -force-language zh-Hans</example>
-        public static string GetForceLanguage()
-        {
-            string value = "";
-            for (int i = 0; i < CommandLineArgs.Length; i++)
-            {
-                if (CommandLineArgs[i] == CommonCommands.MODULE_FORCE_LANGUAGE && i + 1 < CommandLineArgs.Length)
-                {
-                    value = CommandLineArgs[i + 1];
-                    return value;
-                }
-            }
-            
-            return value;
-        }
-
-        /// <summary>
-        /// 如果传入了 <see cref="CommonCommands.MODULE_SHOW_DEBUGGER"/> 参数，则返回 <c>true</c>
-        /// </summary>
-        /// <returns></returns>
-        public static bool GetShowDebugger()
-        {
-            return CommandLineArgs.Any(t => t == CommonCommands.MODULE_SHOW_DEBUGGER);
         }
     }
 }
