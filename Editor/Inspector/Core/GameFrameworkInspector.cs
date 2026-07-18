7c0c853a685adf1a3dcfd24f82924f016758ecc8
Lx34r <zyssmart@live.com>
2026-07-18 10:52:39 +0800

    Add ProcedureLauncherAttribute for filtering available procedure types

--- Editor/Inspector/Core/GameFrameworkInspector.cs
+++ Editor/Inspector/Core/GameFrameworkInspector.cs
@@ -1,27 +1,27 @@
 using UnityEditor;
 
-namespace Moirai.Atropos.Editor.Inspector
+namespace Moirai.Atropos.Editor
 {
     /// <summary>
     /// 游戏框架 Inspector 抽象类。
     /// </summary>
     public abstract class GameFrameworkInspector : UnityEditor.Editor
     {
-        private bool m_IsCompiling = false;
+        private bool _isCompiling = false;
 
         /// <summary>
         /// 绘制事件。
         /// </summary>
         public override void OnInspectorGUI()
         {
-            if (m_IsCompiling && !EditorApplication.isCompiling)
+            if (_isCompiling && !EditorApplication.isCompiling)
             {
-                m_IsCompiling = false;
+                _isCompiling = false;
                 OnCompileComplete();
             }
-            else if (!m_IsCompiling && EditorApplication.isCompiling)
+            else if (!_isCompiling && EditorApplication.isCompiling)
             {
-                m_IsCompiling = true;
+                _isCompiling = true;
                 OnCompileStart();
             }
         }
