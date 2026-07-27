1a04a1faaa06859c5167eb71eef6c2df1116918b
Lx34r <zyssmart@live.com>
2026-07-27 22:53:29 +0800

    Rename EScaleModes to EScaleMode and add Action property with setter

--- Runtime/Modules/Input/InputHandler/UnityInputSystem/Prompts/PromptActionIcon.cs
+++ Runtime/Modules/Input/InputHandler/UnityInputSystem/Prompts/PromptActionIcon.cs
@@ -12,36 +12,53 @@ namespace Moirai.Atropos.Input.Prompts
     [RequireComponent(typeof(Image))]
     public class PromptActionIcon : InputSystemPromptBase
     {
-        private enum EScaleModes { None, Height, Width }
+        private enum EScaleMode { None, Height, Width }
 
         // Bindings
         private const string BINDINGS_GROUP = "绑定 [Bindings]";
-        
+
         [BoxGroup(BINDINGS_GROUP), ReadOnly]
         [Tooltip("要应用提示 Sprite 的图像")]
         [SerializeField] private Image m_Image;
 
         // Settings
         private const string SETTINGS_GROUP = "设置 [Settings]";
-        
+
         [BoxGroup(SETTINGS_GROUP), Required]
-        [Tooltip("完整路径，包括绑定映射和动作，例如 \"Player/Move\"")]
+        [Tooltip("完整路径，包括绑定映射和动作，例如 Player/Move")]
         [SerializeField] private string m_Action = "Player/Move";
-        
+
         [BoxGroup(SETTINGS_GROUP)]
         [Tooltip("是否将复合动作替换位单个的组合图标")]
         [SerializeField] private bool m_IsComposite = false;
-        
+
         [BoxGroup(SETTINGS_GROUP)]
         [Tooltip("将UI元素的大小设置为其原始图像的大小")]
         [SerializeField] private bool m_SetNativeSize = false;
         [BoxGroup(SETTINGS_GROUP)]
         [Tooltip("设置其原始大小时，缩放基于的基准")]
         [ShowIf(nameof(m_SetNativeSize))]
-        [SerializeField] private EScaleModes m_ScaleMode = EScaleModes.Height;
+        [SerializeField] private EScaleMode m_ScaleMode = EScaleMode.Height;
 
         protected override bool IsValid => m_Image != null;
-        
+
+        /// <summary>
+        /// 获取设置绑定的按键。
+        /// </summary>
+        /// <param name="value">按键的完整路径</param>
+        /// <remarks>包括绑定映射和动作，例如 Player/Move</remarks>
+        public string Action
+        {
+            get => m_Action;
+            set
+            {
+                if (m_Action == value) return;
+
+                m_Action = value;
+                RefreshPrompt();
+            }
+        }
+
         private void Reset()
         {
             m_Image ??= GetComponent<Image>();
@@ -57,12 +74,12 @@ namespace Moirai.Atropos.Input.Prompts
             m_Image.sprite = sourceSprite;
             if (m_SetNativeSize)
             {
-                var originalValue = m_ScaleMode == EScaleModes.Height ? m_Image.rectTransform.rect.height : m_Image.rectTransform.rect.width;
+                var originalValue = m_ScaleMode == EScaleMode.Height ? m_Image.rectTransform.rect.height : m_Image.rectTransform.rect.width;
                 m_Image.SetNativeSize();
 
-                if (m_ScaleMode != EScaleModes.None)
+                if (m_ScaleMode != EScaleMode.None)
                 {
-                    float scale = originalValue / (m_ScaleMode == EScaleModes.Height ? m_Image.rectTransform.rect.height : m_Image.rectTransform.rect.width);
+                    float scale = originalValue / (m_ScaleMode == EScaleMode.Height ? m_Image.rectTransform.rect.height : m_Image.rectTransform.rect.width);
                     m_Image.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, m_Image.rectTransform.rect.height * scale);
                     m_Image.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, m_Image.rectTransform.rect.width * scale);
                 }
