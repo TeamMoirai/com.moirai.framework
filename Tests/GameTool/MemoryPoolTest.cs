cfc1a53636eaae43f4d302e5bc8b00d56cb8a29e
Lx34r <zyssmart@live.com>
2026-08-14 18:45:28 +0800

    调整 测试脚本命名空间

--- Tests/GameTool/MemoryPoolTest.cs
+++ Tests/GameTool/MemoryPoolTest.cs
@@ -2,7 +2,7 @@ using System;
 using Moirai.Atropos;
 using NUnit.Framework;
 
-namespace Tests
+namespace GameTool
 {
     public class MemoryPoolTest
     {
--- Tests/GameTool/ModuleSystemTest.cs
+++ Tests/GameTool/ModuleSystemTest.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Tests
+namespace GameTool
 {
     [TestFixture]
     public class ModuleSystemTest
