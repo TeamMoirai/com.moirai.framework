cb69023c43a88b1d3855a4a3c81acc9fd70e17b1
Lx34r <zyssmart@live.com>
2026-09-12 15:50:35 +0800

    fix: property.GetValue(propertyName) 把属性名字符串当实例传入，静态属性必抛 TargetException

--- Runtime/Core/Utilities/ReflectionUtility.cs
+++ Runtime/Core/Utilities/ReflectionUtility.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -260,7 +260,7 @@ namespace Moirai.Atropos
                 throw new NullReferenceException($"Type : {type} can not find property: {propertyName} !");
             }
 
-            return property.GetValue(propertyName);
+            return property.GetValue(null);
         }
 
         /// <summary>
